b19137dc3df345396e8de8e2413420778d3b0eef
Amir Reza Dalir <dalirnet@gmail.com>
2026-01-04 19:44:28 +0330

    refactor: rename RECORD/RECORDS to UNIT/UNITS in Endpoint enum

--- src/runtime/utils/endpoint.ts
+++ src/runtime/utils/endpoint.ts
@@ -1,23 +1,46 @@
 import type { BaseState } from "@harlem/core";
 
-import {
-    StoreEndpoint,
-    StoreEndpointStatus,
-    type StoreEndpointDefinition,
-} from "../types/store";
+import type { ApiAction } from "../core/api";
+
+export enum Endpoint {
+    GET_UNIT = "getUnit",
+    GET_UNITS = "getUnits",
+    POST_UNIT = "postUnit",
+    POST_UNITS = "postUnits",
+    PUT_UNIT = "putUnit",
+    PUT_UNITS = "putUnits",
+    PATCH_UNIT = "patchUnit",
+    PATCH_UNITS = "patchUnits",
+    DELETE_UNIT = "deleteUnit",
+    DELETE_UNITS = "deleteUnits",
+}
+
+export enum EndpointStatus {
+    IDLE = "idle",
+    PENDING = "pending",
+    SUCCESS = "success",
+    FAILED = "failed",
+}
+
+export interface EndpointDefinition {
+    action: ApiAction;
+    url: string | ((keys: Record<PropertyKey, unknown>) => string);
+}
+
+export interface EndpointMemory {
+    status: EndpointStatus;
+}
 
 export function makeEndpointStatusKey<
-    K extends StoreEndpoint,
-    S extends StoreEndpointStatus,
+    K extends Endpoint,
+    S extends EndpointStatus,
 >(key: K, status: S): `${K}Is${Capitalize<S>}` {
     return `${key}Is${status.charAt(0).toUpperCase() + status.slice(1)}` as any;
 }
 
 export function getEndpoint(
-    endpoints:
-        | Partial<Record<StoreEndpoint, StoreEndpointDefinition>>
-        | undefined,
-    key: StoreEndpoint,
+    endpoints: Partial<Record<Endpoint, EndpointDefinition>> | undefined,
+    key: Endpoint,
 ) {
     const endpoint = endpoints?.[key];
     if (!endpoint) {
@@ -42,11 +65,11 @@ export function makeEndpointsStatus<T>(
     getter: (name: string, fn: (state: BaseState) => boolean) => T,
 ) {
     const output = {} as {
-        [K in StoreEndpoint as `${K}Is${Capitalize<StoreEndpointStatus>}`]: T;
+        [K in Endpoint as `${K}Is${Capitalize<EndpointStatus>}`]: T;
     };
 
-    for (const key of Object.values(StoreEndpoint)) {
-        for (const status of Object.values(StoreEndpointStatus)) {
+    for (const key of Object.values(Endpoint)) {
+        for (const status of Object.values(EndpointStatus)) {
             const statusKey = makeEndpointStatusKey(key, status);
 
             output[statusKey] = getter(statusKey, (state) => {
