6a91b0cb2f396faf29f77b75c22e48390c5d720f
Amir Reza Dalir <dalirnet@gmail.com>
2026-02-09 00:22:15 +0330

    refactor: simplify core store module

--- src/runtime/core/store.ts
+++ src/runtime/core/store.ts
@@ -1,6 +1,5 @@
 import { createConsola } from "consola";
-import { createStore as createSourceStore } from "@harlem/core";
-import type { ComputedRef } from "vue";
+import { createStore as createStoreSource } from "@harlem/core";
 
 import { runtimeConfig } from "../config";
 
@@ -8,95 +7,18 @@ import { createModelFactory } from "./layers/model";
 import { createViewFactory } from "./layers/view";
 import { createActionFactory } from "./layers/action";
 
-import { initializeState, createMutations, createCommitter } from "./utils/model";
-import { createView } from "./utils/view";
-import { createAction } from "./utils/action";
+import { createStoreState, createStoreModel, createStoreView, createStoreAction } from "./utils/store";
 
-import type { Model, ModelFactory, Mutations } from "./types/model";
-import type { ViewDefinitions, ViewResult, ViewFactory } from "./types/view";
-import {
-    type Action,
-    type ActionApiChain,
-    type ActionCommitChain,
-    type ActionCommitter,
-    type ActionDefinition,
-    type ActionDefinitions,
-    type ActionFactory,
-    type ActionHandleChain,
-    DEFINITION,
-} from "./types/action";
-
-export type StoreModel<M extends Model> = ActionCommitter<M>;
-
-export type StoreView<M extends Model, VD extends ViewDefinitions<M>> = {
-    readonly [K in keyof VD]: ComputedRef<ViewResult<M, VD[K]>>;
-};
-
-export type StoreAction<M extends Model, V, AD extends Record<string, ActionDefinition<M, V, unknown>>> = {
-    [K in keyof AD]: Action<V>;
-};
-
-export interface StoreConfig<
-    M extends Model,
-    VD extends ViewDefinitions<M>,
-    _AD extends ActionDefinitions<M, StoreView<M, VD>>,
-> {
-    name: string;
-    model: (factory: ModelFactory) => M;
-    view: (factory: ViewFactory<M>) => VD;
-    action: (
-        factory: ActionFactory<M, StoreView<M, VD>>,
-    ) => Record<
-        string,
-        | ActionApiChain<M, StoreView<M, VD>, unknown>
-        | ActionHandleChain<M, StoreView<M, VD>, unknown>
-        | ActionCommitChain<M, StoreView<M, VD>, unknown>
-    >;
-}
-
-export interface Store<
-    M extends Model,
-    VD extends ViewDefinitions<M>,
-    AD extends ActionDefinitions<M, StoreView<M, VD>>,
-> {
-    model: StoreModel<M>;
-    view: StoreView<M, VD>;
-    action: StoreAction<M, StoreView<M, VD>, AD>;
-}
-
-function createStoreModel<M extends Model>(mutations: Mutations<M>): StoreModel<M> {
-    return createCommitter(mutations);
-}
-
-function createStoreView<M extends Model, VD extends ViewDefinitions<M>>(
-    source: ReturnType<typeof createSourceStore>,
-    viewDefinitions: VD,
-): StoreView<M, VD> {
-    return createView(source, viewDefinitions);
-}
-
-function createStoreAction<
-    M extends Model,
-    VD extends ViewDefinitions<M>,
-    AD extends ActionDefinitions<M, StoreView<M, VD>>,
->(
-    actionDefinitions: Record<string, { readonly [DEFINITION]: ActionDefinition<M, StoreView<M, VD>, unknown> }>,
-    view: StoreView<M, VD>,
-    mutations: Mutations<M>,
-): StoreAction<M, StoreView<M, VD>, AD> {
-    const actions = {} as Record<string, Action<StoreView<M, VD>>>;
-    for (const [key, chain] of Object.entries(actionDefinitions)) {
-        actions[key] = createAction((chain as any)[DEFINITION], mutations, view, key);
-    }
-
-    return actions as StoreAction<M, StoreView<M, VD>, AD>;
-}
+import type { ModelDefinitions } from "./types/model";
+import type { ViewDefinitions } from "./types/view";
+import type { ActionDefinitions } from "./types/action";
+import type { Store, StoreConfig } from "./types/store";
 
 export function createStore<
-    M extends Model,
-    VD extends ViewDefinitions<M>,
-    AD extends ActionDefinitions<M, StoreView<M, VD>>,
->(config: StoreConfig<M, VD, AD>): Store<M, VD, AD> {
+    MD extends ModelDefinitions,
+    VD extends ViewDefinitions<MD>,
+    AD extends ActionDefinitions<MD, VD>,
+>(config: StoreConfig<MD, VD, AD>): Store<MD, VD, AD> {
     const logger = createConsola({
         level: runtimeConfig.logger,
         defaults: {
@@ -104,26 +26,22 @@ export function createStore<
         },
     });
 
-    const modelFactory = createModelFactory(runtimeConfig.model, logger);
-    const viewFactory = createViewFactory<M>(runtimeConfig.view, logger);
-    const actionFactory = createActionFactory<M, StoreView<M, VD>>(runtimeConfig.action, logger);
-
     logger.info("Creating store");
+
+    const modelFactory = createModelFactory(runtimeConfig.model, logger);
+    const viewFactory = createViewFactory<MD>(runtimeConfig.view, logger);
+    const actionFactory = createActionFactory<MD, VD>(runtimeConfig.action, logger);
 
     const modelDefinitions = config.model(modelFactory);
     const viewDefinitions = config.view(viewFactory);
     const actionDefinitions = config.action(actionFactory);
 
-    logger.debug("Initializing store");
+    const state = createStoreState(modelDefinitions);
+    const source = createStoreSource(config.name, state);
 
-    const state = initializeState(modelDefinitions);
-    const source = createSourceStore(config.name, state);
-
-    const mutations = createMutations(source, modelDefinitions);
-
-    const model = createStoreModel(mutations);
-    const view = createStoreView<M, VD>(source, viewDefinitions);
-    const action = createStoreAction<M, VD, AD>(actionDefinitions, view, mutations);
+    const model = createStoreModel(modelDefinitions, source);
+    const view = createStoreView<MD, VD>(viewDefinitions, source);
+    const action = createStoreAction<MD, VD, AD>(actionDefinitions, model, view);
 
     logger.info("Store created");
 
